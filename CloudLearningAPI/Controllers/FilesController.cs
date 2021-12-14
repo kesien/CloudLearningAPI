fa09d0343f3f2f8bbcb60893fa52cba5baa99c3e
Balazs Nobik <balazsnobik@gmail.com>
2021-12-14 14:30:44 +0100

    Fixed authentication bug for FilesController

--- CloudLearningAPI/Controllers/FilesController.cs
+++ CloudLearningAPI/Controllers/FilesController.cs
@@ -1,11 +1,12 @@
 using System.IO;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
 namespace CloudLearningAPI.Controllers
 {
-    [Authorize]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("[controller]")]
     [ApiController]
     public class FilesController : ControllerBase
